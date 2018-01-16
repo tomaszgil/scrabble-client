f0407e3d956a02fb1a16bf1e138a5b0adaea9219
Tomasz Gil <tomaszgil.pom@gmail.com>
2018-01-16 21:32:07 +0100

    Create basic views

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("views/login.fxml"));
+        primaryStage.setTitle("Scrabble Online");
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
