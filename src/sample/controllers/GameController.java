fc054d61275f2ce8bf1cd4a7d46573e3bca5ac8b
Tomasz Gil <tomaszgil.pom@gmail.com>
2018-02-21 16:42:05 +0100

    Add grid view and labels

--- src/sample/controllers/GameController.java
+++ src/sample/controllers/GameController.java
@@ -1,27 +1,54 @@
 package sample.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import sample.State;
+import sample.models.Player;
 
 public class GameController {
+    @FXML
     public Label userScore;
+    @FXML
     public Label userName;
-    public TableView opponentsResults;
+    @FXML
+    public TableView<Player> opponentsResults;
+    @FXML
+    public TableColumn<Player, String> opponentNameColumn;
+    @FXML
+    public TableColumn<Player, Integer> opponentScoreColumn;
+    @FXML
     public AnchorPane boardPane;
+    @FXML
     public AnchorPane lettersPane;
+    @FXML
     public Button exchangeButton;
+    @FXML
     public Button passButton;
+    @FXML
     public Button confirmButton;
+    @FXML
+    public GridPane boardGrid;
+    @FXML
+    public GridPane lettersGrid;
 
-    public void exchange(ActionEvent actionEvent) {
+    @FXML
+    public void initialize() {
+        userName.setText(State.getPlayer().getName());
+        userScore.setText(State.getPlayer().getPoints().toString());
     }
 
-    public void pass(ActionEvent actionEvent) {
+    public void onExchange(ActionEvent actionEvent) {
     }
 
-    public void confirm(ActionEvent actionEvent) {
+    public void onPass(ActionEvent actionEvent) {
+    }
+
+    public void onConfirm(ActionEvent actionEvent) {
     }
 }
