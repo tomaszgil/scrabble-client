487fcc5b825b0990b3958112353d5257648dabb4
kamciokodzi <kamilkolo96@gmail.com>
2018-02-25 19:25:30 +0100

    Try to receive rooms

--- src/sample/controllers/RoomsController.java
+++ src/sample/controllers/RoomsController.java
@@ -12,6 +12,11 @@ import sample.State;
 import sample.models.Room;
 import sample.utils.SceneSwitcher;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+import static sample.Main.connector;
+
 
 public class RoomsController {
 
@@ -27,11 +32,18 @@ public class RoomsController {
     }
 
     @FXML
-    public void initialize() {
+    public void initialize() throws IOException{
+        getRooms();
         nameColumn.setCellValueFactory(new PropertyValueFactory<Room, String>("name"));
         freeSlotsColumn.setCellValueFactory(new PropertyValueFactory<Room, Integer>("freeSlots"));
         roomsData.setAll(State.getRoomList());
         roomList.setItems(roomsData);
+    }
+
+    private void getRooms() throws IOException{
+        char buffor[] = new char[500];
+        connector.inputStreamReader.read(buffor);
+        System.out.println(buffor);
     }
 
     public void play(ActionEvent actionEvent) {
